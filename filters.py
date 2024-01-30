df03a665cdc4cf97a963283a50e10a242e6d0f0f
Simon Howroyd <howroydlsu@gmail.com>
2024-01-30 15:56:48 +0000

    Added reference to filters

--- filters.py
+++ filters.py
@@ -4,6 +4,13 @@ from typing import Union
 
 
 class NonErrorFilter(logging.Filter):
+    """Filter out messages higher than a specific level, e.g. warning and errors.
+
+    Intended so you can have a buffered handler that logs to stdout and a non-buffered handler that logs to stderr, without duplicates.
+
+    Ref: https://github.com/mCodingLLC/VideosSampleCode/blob/master/videos/135_modern_logging/mylogger.py
+    """
+
     def __init__(self, maxlevel: int = None) -> None:
         super().__init__()
         levelno = getattr(logging, maxlevel or "WARNING")
